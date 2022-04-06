37a51e43f1660739c155beb4335dced5dd722ec9
Jet Kan <jetkan.yk@gmail.com>
2022-04-07 01:14:28 +0800

    Update test file

--- assignments/test.py
+++ assignments/test.py
@@ -10,11 +10,11 @@ def load_files(folder):
     in_files = []
     ans_files = []
 
-    for file in os.listdir(folder):
-        if file.endswith(".a"):
-            ans_files.append(file)
-        elif not file.endswith(".out"):
-            in_files.append(file)
+    for entry in os.listdir(folder):
+        if entry.endswith(".a"):
+            ans_files.append(entry)
+        elif not entry.endswith(".out"):
+            in_files.append(entry)
 
     return sorted(in_files), sorted(ans_files)
 
@@ -22,9 +22,8 @@ def load_files(folder):
 def load_source_code(folder):
     for entry in os.listdir(folder):
         if entry.endswith(".py"):
-            return os.path.join(folder, entry)
-
-    raise FileNotFoundError
+            return entry
+    raise FileNotFoundError(f"No .py file found in {folder}")
 
 
 def evaluate(in_file, out_path, ans_path):
@@ -49,20 +48,20 @@ if __name__ == "__main__":
     source_code = load_source_code(folder)
     in_files, ans_files = load_files(test_folder)
     num_tests = len(in_files)
-    print(f"Testing code {source_code} against {num_tests} tests in {test_folder}")
+    print(f"Testing code {source_code} against {num_tests} tests")
 
     num_pass = 0
     for in_file, ans_file in zip(in_files, ans_files):
+        source_code_path = os.path.join(folder, source_code)
         in_path = os.path.join(test_folder, in_file)
         out_path = os.path.join(test_folder, f"{in_file}.out")
         ans_path = os.path.join(test_folder, f"{in_file}.a")
 
-        os.system(f"python {source_code} < {in_path} > {out_path}")
-
+        os.system(f"python {source_code_path} < {in_path} > {out_path}")
         num_pass += evaluate(in_file, out_path, ans_path)
 
     num_fail = num_tests - num_pass
     if num_fail:
-        print(f"Failed {num_pass} / {num_tests} tests")
+        print(f"Failed {num_fail} / {num_tests} tests")
     else:
         print("Passed all tests. Good job!")
