be06a1140be97382081ec2c4c78f36e754d4093a
Jet Kan <jetkan.yk@gmail.com>
2022-04-07 18:17:20 +0800

    Implement Node, build_tree

--- assignments/week1/task2/tree_height.py
+++ assignments/week1/task2/tree_height.py
@@ -6,10 +6,33 @@ sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)  # new thread will get stack of such size
 
 
-class TreeHeight:
+class Node:
+    def __init__(self, index):
+        self.index = index
+        self.children = list()
+
+    def addChild(self, child):
+        self.children.append(child)
+
+    def __str__(self):
+        children = ", ".join(map(str, self.children))
+        return f"Node {self.index}: [{children}]"
+
+
+class Tree:
     def read(self):
         self.n = int(sys.stdin.readline())
-        self.parent = list(map(int, sys.stdin.readline().split()))
+        self.parents = list(map(int, sys.stdin.readline().split()))
+
+    def build_tree(self):
+        self.nodes = [Node(i) for i in range(self.n)]
+
+        for node in range(self.n):
+            parent = self.parents[node]
+            if parent == -1:
+                self.root = node
+            else:
+                self.nodes[parent].addChild(node)
 
     def compute_height(self):
         # Replace this code with a faster implementation
@@ -19,15 +42,26 @@ class TreeHeight:
             i = vertex
             while i != -1:
                 height += 1
-                i = self.parent[i]
+                i = self.parents[i]
             maxHeight = max(maxHeight, height)
         return maxHeight
 
 
+def test():
+    tree = Tree()
+    tree.read()
+    tree.build_tree()
+
+    print(f"Root = {tree.root}")
+    for node in tree.nodes:
+        print(node)
+
+
 def main():
-    tree = TreeHeight()
+    tree = Tree()
     tree.read()
     print(tree.compute_height())
 
 
-threading.Thread(target=main).start()
+# threading.Thread(target=main).start()
+test()
