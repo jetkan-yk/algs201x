4ddf2333daac203967f3e8facaf2c5974974cb86
Jet Kan <jetkan.yk@gmail.com>
2022-04-07 18:48:09 +0800

    Implement recursive tree height

--- assignments/week1/task2/tree_height.py
+++ assignments/week1/task2/tree_height.py
@@ -9,9 +9,9 @@ threading.stack_size(2**27)  # new thread will get stack of such size
 class Node:
     def __init__(self, index):
         self.index = index
-        self.children = list()
+        self.children = []
 
-    def addChild(self, child):
+    def add_child(self, child):
         self.children.append(child)
 
     def __str__(self):
@@ -23,38 +23,24 @@ class Tree:
     def read(self):
         self.n = int(sys.stdin.readline())
         self.parents = list(map(int, sys.stdin.readline().split()))
+        self._build_tree()
 
-    def build_tree(self):
+    def _build_tree(self):
         self.nodes = [Node(i) for i in range(self.n)]
-
-        for node in range(self.n):
-            parent = self.parents[node]
+        for node in self.nodes:
+            parent = self.parents[node.index]
             if parent == -1:
-                self.root = node
+                self.root = self.nodes[node.index]
             else:
-                self.nodes[parent].addChild(node)
+                self.nodes[parent].add_child(node.index)
+
+    def _max_height(self, node: Node):
+        if not node.children:
+            return 1
+        return 1 + max([self._max_height(self.nodes[child]) for child in node.children])
 
     def compute_height(self):
-        # Replace this code with a faster implementation
-        maxHeight = 0
-        for vertex in range(self.n):
-            height = 0
-            i = vertex
-            while i != -1:
-                height += 1
-                i = self.parents[i]
-            maxHeight = max(maxHeight, height)
-        return maxHeight
-
-
-def test():
-    tree = Tree()
-    tree.read()
-    tree.build_tree()
-
-    print(f"Root = {tree.root}")
-    for node in tree.nodes:
-        print(node)
+        return self._max_height(self.root)
 
 
 def main():
@@ -63,5 +49,4 @@ def main():
     print(tree.compute_height())
 
 
-# threading.Thread(target=main).start()
-test()
+threading.Thread(target=main).start()
