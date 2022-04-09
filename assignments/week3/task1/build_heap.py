4a817094d2975de5fffb2fe0f4c28313d24ce85d
Jet Kan <jetkan.yk@gmail.com>
2022-04-09 18:17:58 +0800

    Implement build_heap

--- assignments/week3/task1/build_heap.py
+++ assignments/week3/task1/build_heap.py
@@ -3,36 +3,46 @@
 
 class HeapBuilder:
     def __init__(self):
-        self._swaps = []
+        self._n = 0
         self._data = []
+        self._swaps = []
 
     def read_data(self):
-        n = int(input())
+        self._n = int(input())
         self._data = [int(s) for s in input().split()]
-        assert n == len(self._data)
+        assert self._n == len(self._data)
 
     def write_response(self):
         print(len(self._swaps))
         for swap in self._swaps:
             print(swap[0], swap[1])
 
-    def generate_swaps(self):
-        # The following naive implementation just sorts
-        # the given sequence using selection sort algorithm
-        # and saves the resulting sequence of swaps.
-        # This turns the given array into a heap,
-        # but in the worst case gives a quadratic number of swaps.
-        #
-        # TODO: replace by a more efficient implementation
-        for i in range(len(self._data)):
-            for j in range(i + 1, len(self._data)):
-                if self._data[i] > self._data[j]:
-                    self._swaps.append((i, j))
-                    self._data[i], self._data[j] = self._data[j], self._data[i]
+    def _siftdown(self, i):
+        min_child = i
+
+        left_child = 2 * i + 1
+        if left_child < self._n and self._data[left_child] < self._data[min_child]:
+            min_child = left_child
+
+        right_child = 2 * i + 2
+        if right_child < self._n and self._data[right_child] < self._data[min_child]:
+            min_child = right_child
+
+        if i != min_child:
+            self._swap(i, min_child)
+            self._siftdown(min_child)
+
+    def _swap(self, i, j):
+        self._data[i], self._data[j] = self._data[j], self._data[i]
+        self._swaps.append((i, j))
+
+    def heapify(self):
+        for i in reversed(range(self._n // 2)):
+            self._siftdown(i)
 
     def solve(self):
         self.read_data()
-        self.generate_swaps()
+        self.heapify()
         self.write_response()
 
 
