788129ed9226f443854f9e2f7b092755bd685393
Jet Kan <jetkan.yk@gmail.com>
2022-04-15 12:45:31 +0800

    Implement hash chain

--- assignments/week4/task2/hash_chains.py
+++ assignments/week4/task2/hash_chains.py
@@ -16,8 +16,7 @@ class QueryProcessor:
 
     def __init__(self, bucket_count):
         self.bucket_count = bucket_count
-        # store all strings in one list
-        self.elems = []
+        self.elems = [[] for _ in range(self.bucket_count)]
 
     def _hash_func(self, s):
         ans = 0
@@ -29,30 +28,29 @@ class QueryProcessor:
         print("yes" if was_found else "no")
 
     def write_chain(self, chain):
-        print(" ".join(chain))
+        print(" ".join(reversed(chain)))
 
     def read_query(self):
         return Query(input().split())
 
     def process_query(self, query):
         if query.type == "check":
-            # use reverse order, because we append strings to the end
-            self.write_chain(
-                cur for cur in reversed(self.elems) if self._hash_func(cur) == query.ind
-            )
+            self.write_chain(self.elems[query.ind])
         else:
-            try:
-                ind = self.elems.index(query.s)
-            except ValueError:
-                ind = -1
-            if query.type == "find":
-                self.write_search_result(ind != -1)
-            elif query.type == "add":
-                if ind == -1:
-                    self.elems.append(query.s)
+            string = query.s
+            hash_value = self._hash_func(string)
+            has_string = string in self.elems[hash_value]
+
+            if query.type == "add":
+                if not has_string:
+                    self.elems[hash_value].append(string)
+            elif query.type == "del":
+                if has_string:
+                    self.elems[hash_value].remove(string)
+            elif query.type == "find":
+                self.write_search_result(has_string)
             else:
-                if ind != -1:
-                    self.elems.pop(ind)
+                raise ValueError
 
     def process_queries(self):
         n = int(input())
