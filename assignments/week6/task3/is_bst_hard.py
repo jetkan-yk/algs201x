b2f6fbf2e367137b864d78661c3974ae1575ab21
Jet Kan <jetkan.yk@gmail.com>
2022-04-16 16:06:39 +0800

    Fix style

--- assignments/week6/task3/is_bst_hard.py
+++ assignments/week6/task3/is_bst_hard.py
@@ -2,22 +2,26 @@
 
 import sys, threading
 
-sys.setrecursionlimit(10**7) # max depth of recursion
-threading.stack_size(2**25)  # new thread will get stack of such size
+sys.setrecursionlimit(10**7)  # max depth of recursion
+threading.stack_size(2**27)  # new thread will get stack of such size
 
-def IsBinarySearchTree(tree):
-  # Implement correct algorithm here
-  return True
+ROOT_NODE = 0
+
+
+def is_bst(tree):
+    # Implement correct algorithm here
+    return True
 
 
 def main():
-  nodes = int(sys.stdin.readline().strip())
-  tree = []
-  for i in range(nodes):
-    tree.append(list(map(int, sys.stdin.readline().strip().split())))
-  if IsBinarySearchTree(tree):
-    print("CORRECT")
-  else:
-    print("INCORRECT")
+    nodes = int(sys.stdin.readline().strip())
+    tree = []
+    for i in range(nodes):
+        tree.append(list(map(int, sys.stdin.readline().strip().split())))
+    if is_bst(tree):
+        print("CORRECT")
+    else:
+        print("INCORRECT")
+
 
 threading.Thread(target=main).start()
