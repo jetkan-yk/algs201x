ec2bab0b35eb408b7e3fdcfb30dea23972717f04
Jet Kan <jetkan.yk@gmail.com>
2022-04-16 15:57:23 +0800

    Implement is_bst

--- assignments/week6/task2/is_bst.py
+++ assignments/week6/task2/is_bst.py
@@ -3,12 +3,33 @@
 import sys, threading
 
 sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**25)  # new thread will get stack of such size
+threading.stack_size(2**27)  # new thread will get stack of such size
+
+ROOT_NODE = 0
 
 
 def is_bst(tree):
-    # Implement correct algorithm here
-    return True
+    def visit(node):
+        if node == -1:
+            return True
+        key, left_child, right_child = tree[node]
+
+        visit(left_child)
+        in_order.append(key)
+        visit(right_child)
+
+    def is_sorted(lst):
+        for i in range(1, len(lst)):
+            if lst[i - 1] > lst[i]:
+                return False
+        return True
+
+    if len(tree) < 2:
+        return True
+
+    in_order = []
+    visit(ROOT_NODE)
+    return is_sorted(in_order)
 
 
 def main():
