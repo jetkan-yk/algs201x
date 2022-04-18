583f25e497b64fe53bbcac25cc27a61b2abfb353
Jet Kan <jetkan.yk@gmail.com>
2022-04-18 21:23:56 +0800

    Fix style

--- assignments/week1/task3/process_packages.py
+++ assignments/week1/task3/process_packages.py
@@ -1,46 +1,53 @@
 # python3
 
+
 class Request:
     def __init__(self, arrival_time, process_time):
         self.arrival_time = arrival_time
         self.process_time = process_time
+
 
 class Response:
     def __init__(self, dropped, start_time):
         self.dropped = dropped
         self.start_time = start_time
 
+
 class Buffer:
     def __init__(self, size):
         self.size = size
         self.finish_time_ = []
 
-    def Process(self, request):
+    def process(self, request):
         # write your code here
         return Response(False, -1)
 
-def ReadRequests(count):
+
+def read_requests(count):
     requests = []
     for i in range(count):
         arrival_time, process_time = map(int, input().strip().split())
         requests.append(Request(arrival_time, process_time))
     return requests
 
-def ProcessRequests(requests, buffer):
+
+def process_requests(requests, buffer):
     responses = []
     for request in requests:
-        responses.append(buffer.Process(request))
+        responses.append(buffer.process(request))
     return responses
 
-def PrintResponses(responses):
+
+def print_responses(responses):
     for response in responses:
         print(response.start_time if not response.dropped else -1)
 
+
 if __name__ == "__main__":
     size, count = map(int, input().strip().split())
-    requests = ReadRequests(count)
+    requests = read_requests(count)
 
     buffer = Buffer(size)
-    responses = ProcessRequests(requests, buffer)
+    responses = process_requests(requests, buffer)
 
-    PrintResponses(responses)
+    print_responses(responses)
