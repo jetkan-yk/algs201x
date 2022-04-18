c565619fa72075f6b86c10845475238288d62095
Jet Kan <jetkan.yk@gmail.com>
2022-04-18 22:34:14 +0800

    Fix style

--- assignments/week3/task3/merging_tables.py
+++ assignments/week3/task3/merging_tables.py
@@ -8,24 +8,26 @@ rank = [1] * n
 parent = list(range(0, n))
 ans = max(lines)
 
-def getParent(table):
+
+def get_parent(table):
     # find parent and compress path
     return parent[table]
 
-def merge(destination, source):
-    realDestination, realSource = getParent(destination), getParent(source)
 
-    if realDestination == realSource:
+def merge(destination, source):
+    read_destination, real_source = get_parent(destination), get_parent(source)
+
+    if read_destination == real_source:
         return False
 
     # merge two components
-    # use union by rank heuristic 
+    # use union by rank heuristic
     # update ans with the new maximum table size
-    
+
     return True
+
 
 for i in range(m):
     destination, source = map(int, sys.stdin.readline().split())
     merge(destination - 1, source - 1)
     print(ans)
-    
