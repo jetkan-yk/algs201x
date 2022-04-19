7aa37a41dc31b5c6dfd89a82d4332a4c36d9c2d3
Jet Kan <jetkan.yk@gmail.com>
2022-04-19 14:53:48 +0800

    Implement merging tables

--- assignments/week3/task3/merging_tables.py
+++ assignments/week3/task3/merging_tables.py
@@ -9,22 +9,34 @@ parent = list(range(0, n))
 ans = max(lines)
 
 
-def get_parent(table):
-    # find parent and compress path
-    return parent[table]
+def get_parent(i):
+    # use path compression heuristic
+    if i != parent[i]:
+        parent[i] = get_parent(parent[i])
+    return parent[i]
 
 
-def merge(destination, source):
-    read_destination, real_source = get_parent(destination), get_parent(source)
-
-    if read_destination == real_source:
-        return False
-
-    # merge two components
+def merge(i, j):
     # use union by rank heuristic
-    # update ans with the new maximum table size
+    i, j = get_parent(i), get_parent(j)
 
-    return True
+    if i == j:
+        return
+
+    global ans
+    if rank[i] > rank[j]:
+        parent[j] = i
+        lines[i] += lines[j]
+        lines[j] = 0
+        ans = max(ans, lines[i])
+    else:
+        parent[i] = j
+        lines[j] += lines[i]
+        lines[i] = 0
+        ans = max(ans, lines[j])
+
+        if rank[i] == rank[j]:
+            rank[j] += 1
 
 
 for i in range(m):
