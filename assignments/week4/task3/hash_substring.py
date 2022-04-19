4b26c21d0d6f634bdf9bb933720ba4db59e95cc3
Jet Kan <jetkan.yk@gmail.com>
2022-04-19 19:48:49 +0800

    Implement hash substring

--- assignments/week4/task3/hash_substring.py
+++ assignments/week4/task3/hash_substring.py
@@ -5,16 +5,46 @@ def read_input():
     return (input().rstrip(), input().rstrip())
 
 
+def poly_hash(S, p, x):
+    hash = 0
+    for c in reversed(S):
+        hash = (hash * x + ord(c)) % p
+    return hash
+
+
+def precompute_hashes(T, P_len, p, x):
+    T_len = len(T)
+    H = [None] * (T_len - P_len + 1)
+    S = T[P_len:]
+    H[T_len - P_len] = poly_hash(S, p, x)
+
+    y = 1
+    for _ in range(P_len):
+        y = y * x % p
+
+    for i in range(T_len - P_len - 1, -1, -1):
+        H[i] = (x * H[i + 1] + ord(T[i]) - y * ord(T[i + P_len])) % p
+    return H
+
+
 def print_occurrences(output):
     print(" ".join(map(str, output)))
 
 
 def get_occurrences(pattern, text):
-    return [
-        i
-        for i in range(len(text) - len(pattern) + 1)
-        if text[i : i + len(pattern)] == pattern
-    ]
+    prime = 1000000007
+    multiplier = 263
+
+    positions = []
+    p_hash = poly_hash(pattern, prime, multiplier)
+    hashes = precompute_hashes(text, len(pattern), prime, multiplier)
+
+    for i in range(len(text) - len(pattern) + 1):
+        if p_hash != hashes[i]:
+            continue
+        if text[i : i + len(pattern)] == pattern:
+            positions.append(i)
+    return positions
 
 
 if __name__ == "__main__":
